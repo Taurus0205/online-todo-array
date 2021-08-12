023a50dc349071c350294272da5aca3bfb5fc2d8
Taurus0205 <dolchin1990@mail.ru>
2021-08-12 16:37:25 +0500

    Add new array to use online array

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-const elHumbergerBtn = document.querySelector(".hamburger-btn");
-const elHeader = document.querySelector(".header");
+const elForm = document.querySelector(".todo-form");
+const elInput = elForm.querySelector(".todo-input");
+const elTodoList = document.querySelector(".todo-list");
 
-elHumbergerBtn.addEventListener("click", function () {
-  elHeader.classList.toggle("header--adoptive");
-});
+const elTemplate = document.querySelector("#todo-item--template").content;
+
+// async array
+
+// const todosArr = fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then(data);
+
+async function fetchTodos() {
+  const reponse = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  const data = await reponse.json();
+  renderTemplate(data, elTodoList);
+}
+fetchTodos();
+let newArrey = [];
+// render
+function renderTemplate(todoArr, element) {
+  newArrey = todoArr;
+  todoArr.forEach((row) => {
+    const todoTemplate = elTemplate.cloneNode(true);
+    const elTodoSpan = todoTemplate.querySelector(".todo-item-complete-text");
+    const eltodoChecked = todoTemplate.querySelector(".todo-input-complete");
+    const eltodoDeleteBtn = todoTemplate.querySelector(".todo-item-delete-btn");
+
+    elTodoSpan.textContent = row.title;
+    eltodoChecked.dataset.todoId = row.id;
+    eltodoDeleteBtn.dataset.todoId = row.id;
+    eltodoChecked.checked = row.completed;
+
+    element.appendChild(todoTemplate);
+
+    eltodoChecked.addEventListener("click", (evt) => {
+      const todoId = evt.target.dataset.todoId;
+      const checkedTodo = todoArr.find((row) => row.id == todoId);
+      checkedTodo.checked = !checkedTodo.checked;
+    });
+    if (eltodoChecked.checked === true) {
+      elTodoSpan.classList.add("done");
+    }
+  });
+}
